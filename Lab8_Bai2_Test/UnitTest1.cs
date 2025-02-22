4098b7cff45baaf65c98f5a4143c8ddbfd2d58f6
Huan Huynh <151844146+HuanHuynh161021@users.noreply.github.com>
2025-02-22 23:18:47 +0700

    Initial commit: Tạo solution với 2 projects

---
+++ Lab8_Bai2_Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Lab8_Bai2_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
